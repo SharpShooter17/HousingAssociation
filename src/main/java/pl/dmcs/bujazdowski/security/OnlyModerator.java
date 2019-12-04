1fcad9791404bbc20d2860a90e9798db05f2ed70
Bartosz Ujazdowski <bartosz.ujazdowski@comarch.com>
2019-12-04 18:10:28 +0100

    BugFixing

--- src/main/java/pl/dmcs/bujazdowski/security/OnlyModerator.java
+++ src/main/java/pl/dmcs/bujazdowski/security/OnlyModerator.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('ADMINISTRATOR')")
+@PreAuthorize("hasAuthority('MODERATOR')")
 public @interface OnlyModerator {
 }
