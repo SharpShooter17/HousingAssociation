520f0c63ebcd12f1c75f5068539af4a0daca1b8d
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2020-01-10 10:39:58 +0100

    Spring test configuration'

--- src/test/java/pl/dmcs/bujazdowski/configuration/ConfigTest.java
+++ src/test/java/pl/dmcs/bujazdowski/configuration/ConfigTest.java
@@ -1,38 +1,46 @@
 package pl.dmcs.bujazdowski.configuration;
 
+
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"pl.dmcs.bujazdowski.controller", "pl.dmcs.bujazdowski.dao", "pl.dmcs.bujazdowski.service"})
+@EnableScheduling
 @EnableJpaRepositories(basePackages = "pl.dmcs.bujazdowski.dao")
 @EnableTransactionManagement
-public class HibernatePersistenceConfiguration {
+public class ConfigTest {
 
     @Primary
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/spring?characterEncoding=utf-8");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("password");
 
         return dataSource;
     }
@@ -41,12 +49,11 @@ public class HibernatePersistenceConfiguration {
         Properties properties = new Properties();
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.hbm2ddl.auto", "update");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         properties.put("hibernate.default_schema", "public");
         return properties;
     }
 
-    @DependsOn("flyway")
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
@@ -71,14 +78,15 @@ public class HibernatePersistenceConfiguration {
         return new PersistenceExceptionTranslationPostProcessor();
     }
 
-    @Bean(initMethod = "migrate")
-    public Flyway flyway(@Autowired DataSource dataSource) {
-        return Flyway.configure()
-                .dataSource(dataSource)
-                .encoding("UTF-8")
-                .locations("classpath:db/migration")
-                .baselineOnMigrate(true)
-                .installedBy("Bartosz Ujazdowski")
-                .load();
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
--- src/test/java/pl/dmcs/bujazdowski/controller/page/user/UserControllerTest.java
+++ src/test/java/pl/dmcs/bujazdowski/controller/page/user/UserControllerTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.web.WebAppConfiguration;
-import pl.dmcs.bujazdowski.configuration.Spring5Configuration;
+import pl.dmcs.bujazdowski.configuration.ConfigTest;
 
+@SpringJUnitConfig(ConfigTest.class)
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {TestContext.class, Spring5Configuration.class})
+@ContextConfiguration(classes = ConfigTest.class)
 @WebAppConfiguration
 class UserControllerTest {
 
